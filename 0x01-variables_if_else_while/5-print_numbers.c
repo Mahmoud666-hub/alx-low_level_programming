b0647f72c224988bb165040fa733ae4ff72d394e
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-16 09:32:52 -0800

    5555

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*describe*/
+
+	int w = 0;
+
+	while (w < 10)
+	{
+		printf("%d", w);
+		w++;
+	}
+	printf("\n");
+	return (0);
+
+}
