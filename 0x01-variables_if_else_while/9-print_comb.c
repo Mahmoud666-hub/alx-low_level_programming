80b70ce1b5ae3bb198f77e432d9f58081fe471f1
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-16 11:44:11 -0800

    999999

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*describe*/
+	int o = 48;
+
+	while (o <= 57)
+	{
+	putchar(o);
+	putchar(',');
+	putchar(' ');
+	o++;
+	} return (0);
+}
