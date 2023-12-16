0bff9272935674d75f537dbf2a2a5a2c01ce3ada
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-15 18:54:25 -0800

    222233

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+	/*describe*/
+{
+	char q = 'a';
+		while (q <= 'z')
+		{
+			putchar(q);
+			q++;
+		}
+		return (0);
+}
