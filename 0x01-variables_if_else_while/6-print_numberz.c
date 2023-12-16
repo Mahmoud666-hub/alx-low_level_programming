88b553093e9bfea504b2cca6ce14f63ee0872c17
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-16 09:47:33 -0800

    666666

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*describbe*/
+	int v;
+
+	for (v = 48; v <= 57; v++)
+	{
+		putchar(v);
+	}
+	putchar('\n');
+	return (0);
+}
