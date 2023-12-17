42c30c1fa815f1d0ae92a8149a55443ef7c631e7
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-17 14:29:30 -0800

    101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*describe*/
+	int q = 48;
+	int w = (q + 1);
+	int e = (q + 2);
+
+	for (q = 48; q < 56; q++)
+	{
+		for (w = (q + 1); w < 57; w++)
+		{
+			for (e = (q + 2); e <= 57; e++)
+			{
+				putchar(q);
+				putchar(w);
+				putchar(e);
+
+				if ((q != 55) || (w != 56) || (e != 57))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	} putchar('\n');
+	return (0);
+}
