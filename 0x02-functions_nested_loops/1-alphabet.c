3a44be050b685ee22ada99b2e676c5a981e4fcf7
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-19 17:19:59 -0800

    httfrr1111

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	/*describe*/
+	char c;
+	for (c = 97; c <= 122; c++ )
+	{
+		_putchar(c);
+	} _putchar('\n');
+}
