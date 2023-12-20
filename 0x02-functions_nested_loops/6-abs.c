c3f8d1d6982325d4154bf978bdc61d90c48d7d52
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-19 19:54:50 -0800

    5679kl

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - tests whether a character is a
+ * lowercase letter from the English alphabet.
+ * @c: character to test.
+ * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
+ */
+int _abs(int c)
+{
+	int n;
+
+	if (c < 0)
+	{
+		n = (-1) * n;
+	} return (n);
+}
