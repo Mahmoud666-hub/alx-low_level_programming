66e7c8a4c1831dcc517d77c71aac3cf96c0637f5
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-19 16:50:05 -0800

    0task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*describe*/
+	int x = 0;
+	char c[] = "_putchar\n";
+
+	while (x <= 8)
+	{
+		_putchar(c[x]);
+		x++;
+	}
+	return (0);
+}
