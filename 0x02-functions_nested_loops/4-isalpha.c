44a5ac409f1f64cb5ff80cb619f8f76cd2b62dab
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-19 19:07:47 -0800

    76hy

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,13 +8,9 @@
 int _isalpha(int c)
 {
 	/*des*/
-
 	if (c >= 65 && c <= 90)
-	{
 		return (1);
-	}
-       if (c >= 97 && c <= 122)
-       {
-	       return (1);
-       } return (0);
+	if (c >= 97 && c <= 122)
+	{ return (1);
+	} return (0);
 }
