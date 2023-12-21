ecfcb759ad61c2a0916d4a14176bd29c8ce661e2
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-21 10:42:20 -0800

    1111154rersws

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,6 @@ void print_to_98(int n)
 	if (n == 98)
 	{
 		printf("98");
-		printf("\n");
 	}
 	else if (n < 98)
 	{
@@ -25,7 +24,6 @@ void print_to_98(int n)
 				printf(", ");
 			}
 		}
-		puts("\n");
 	}
 	else if (n > 98)
 	{
