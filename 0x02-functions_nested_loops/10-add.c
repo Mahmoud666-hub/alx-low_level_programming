707b4ff8892bbf1a278c262fad21648b633f5b44
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-21 05:55:41 -0800

    4510

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers together
+ * @x :- parameter intiger
+ * @y :- parameter intiger
+ * return :- (z) for result
+ */
+int add(int x, int y)
+{
+	/*des*/
+	int z;
+
+	z = (x + y);
+	return (z);
+}
