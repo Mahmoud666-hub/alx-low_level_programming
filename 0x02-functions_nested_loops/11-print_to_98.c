d406ee6f88370f2d2cb36965e681f7c1d8f31126
Mahmoud <ramadanmahmoud945@gmail.com>
2023-12-21 10:34:32 -0800

    1111154rer

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - count the natural numbers from n to 98.
+ * @n: value to count from.
+ *
+ */
+void print_to_98(int n)
+{
+	/*des*/
+	if (n == 98)
+	{
+		printf("98");
+		printf("\n");
+	}
+	else if (n < 98)
+	{
+		int x;
+
+		for (x = n; x <= 98; x++)
+		{
+			printf("%d", x);
+			if (x != 98)
+			{
+				printf(", ");
+			}
+		}
+		puts("\n");
+	}
+	else if (n > 98)
+	{
+		int y;
+
+		for (y = n; y >= 98; y--)
+		{
+			printf("%d", y);
+			if (y != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
