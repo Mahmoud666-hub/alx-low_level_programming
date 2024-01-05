ca29c4197312a71df2ebb85e1e6e6c4bd98a4492
Mahmoud <ramadanmahmoud945@gmail.com>
2024-01-04 18:46:25 -0800

    gtkkkr5

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array
+ * @n: parameter
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int x;
+	int r;
+	r = n;
+	int *t;
+
+	for (x = 0; x <= n; x++)
+	{
+		t[x] == a[x];
+		a[x] == a[r];
+		a[r] == t[x];
+		r--;
+	}
+}
---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - change lowercase letters to uppercase.
+ * @s: analized string.
+ * Return: String with all letters Uppercased.
+ */
+char *string_toupper(char *q)
+{
+	int m = 0;
+
+	while (*(q + m) != '\0')
+	{
+		if (*(q + m) >= 'a' && *(q + m) <= 'z')
+		{
+			*(q + m) = *(q + m) - 32;
+		}
+		m++;
+	}
+	return (q);
+}
