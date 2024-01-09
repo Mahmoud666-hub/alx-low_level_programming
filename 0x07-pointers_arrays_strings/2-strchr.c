9b5307388aeb6eea12e379245451f2648688372c
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-10 00:16:04 +0200

    22

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,7 +8,7 @@
 char *_strchr(char *s, char c)
 {
 int i = 0;
-char *p = {'\0'};
+char *p = {0};
 while (s[i] != '\0')
 {
 if (s[i] == c)
@@ -19,9 +19,8 @@ break;
 }
 else if (c == '\0')
 {
-p = '\0';
+p = s;
 }
-
 i++;
 }
 return (p);
