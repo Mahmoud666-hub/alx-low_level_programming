7f697d060fd91151d7e64015d805517762b9d282
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-10 01:23:53 +0200

    3333323333

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: pointer
+ * @accept: pointer
+ * Return: integer
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+    int i = 0;
+    while (s[i] != '\0')
+    {
+        if (s[i] == accept[0])
+        {
+            return (i + 1);
+        } i++;
+        
+    }
+    return (0);
+}
