b47be2b9c185b54573f8ec03871ce3cb66622866
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-11 20:08:28 +0200

    2222322

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,14 +6,10 @@
 */
 int _strlen_recursion(char *s)
 {
-    if (s[i] == '\0')
-    {
-        return (i);
-    }
-    else
-    {
-        s++;
-        i = i + _strlen_recursion(s);
-    }
-    
+if (*s == '\0')
+{
+return (0);
+}
+else
+return (1 + (_strlen_recursion(s + 1)));
 }
---
+++ 0x08-recursion/nono.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    char *s = "hello people";
+    while ((*s) != '\0')
+    {
+        /* code */s++;
+    }
+    printf("%c \n", *(s-2));
+    int m = ;
+    printf("%d\n", m);
+    
+    
+    
+}
