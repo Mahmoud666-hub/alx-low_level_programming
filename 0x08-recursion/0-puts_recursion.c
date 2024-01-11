f23e4ae28e5b94342a1526e99e05b46df89a6871
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-11 03:43:14 +0200

    0030000

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,17 +6,16 @@
 */
 void _puts_recursion(char *s)
 {
-    int i = 0;
-    if (s[i] == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
-    else
-    {
-        _putchar(s[i]);
-        s++;
-        _puts_recursion(s);
-    }
-    
+int i = 0;
+if (s[i] == '\0')
+{
+_putchar('\n');
+return;
+}
+else
+{
+_putchar(s[i]);
+s++;
+_puts_recursion(s);
+}
 }
