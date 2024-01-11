04f20d3d73a17b736a46eb913ab5666ab39d82a6
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-12 01:14:38 +0200

    555555

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,45 @@
 #include"main.h"
 /**
- * _sqrt_recursion - returns the natural square root of a number
+ * troy - returns result of square root to other
+ * @n: parameter
+ * @b: parameter
+ * Return: integer
+*/
+int troy(int n, int b)
+{
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0)
+{
+return (0);
+}
+if (n == b * b)
+{
+return (b);
+}
+else
+{
+if (n == b)
+{
+return (-1);
+}
+return (troy(n, b + 1));
+}
+}
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number.
  * @n: parameter
  * Return: integer
 */
-int _sqrt_recursion(int n)   
+int _sqrt_recursion(int n)
+{
+int m;
+int b = 0;
+b++;
+m = troy(n, b);
+return (m);
+}
--- 0x08-recursion/nono.c
+++ 0x08-recursion/nono.c
@@ -1,15 +1,41 @@
-#include<stdio.h>
-void main()
+#include"main.h"
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: parameter
+ * Return: integer
+*/
+#include"main.h"
+int test(int x, int d);
+
+int _sqrt_recursion(int n)
 {
-    char *s = "hello people";
-    while ((*s) != '\0')
+    int d = 0;
+    d++;
+    int m = test(n,d);
+    return (m);
+}
+int test(int x, int d)
+{
+    if (x < 0)
     {
-        /* code */s++;
+        return (-1);
     }
-    printf("%c \n", *(s-2));
-    int m = ;
-    printf("%d\n", m);
     
-    
-    
+    if (d * d == x)
+    {
+        return d;
+    }
+    else
+    {
+       if (d != x)
+       {
+        test(x, d+1);
+       }
+       else
+       {
+        return (-1);
+       }
+        
+    }
+        
 }
