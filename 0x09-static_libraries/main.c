944168f8cea73b8c8177d50b80d7e7475357a81c
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-14 21:57:45 +0200

    000100010

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,10 @@
 #include "main.h"
-
+/**
+ * main - wsde vfde
+ * Return: integer
+*/
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+_puts("\"At the end of the day,");
 	return (0);
 }
