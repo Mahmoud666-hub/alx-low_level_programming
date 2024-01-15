320c0bab5a11359bf01c0c2d053a63e0d2a6cbab
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-15 14:44:01 +0200

    1111101111

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - print number of arguments
+ * @argc: parameter
+ * @argv: parameter
+ * Return: integer
+*/
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc - 1);
+(void)argv;
+return (0);
+}
