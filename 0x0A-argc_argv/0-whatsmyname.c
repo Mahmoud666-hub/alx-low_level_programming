ade1070013c4ffaf37d0055452fd2d59f06ebe98
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-15 14:34:37 +0200

    0000100010

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main -print name
+ * @argc: parameter
+ * @argv: parameter
+ * Return: integer
+*/
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+(void)argc;
+return (0);
+}
