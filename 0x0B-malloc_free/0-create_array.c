99cd1206c5e44c00edf7e49600bd52d2ac55511c
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-16 22:20:22 +0200

    0000090

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,6 +8,11 @@
 char *create_array(unsigned int size, char c)
 { unsigned int i;
 char *p = malloc(size * sizeof(char));
+if (p == NULL)
+{
+free(p);
+exit(0);
+}
 if (size == 0)
 {
 return ('\0');
@@ -15,11 +20,6 @@ return ('\0');
 for (i = 0; i < size; i++)
 {
 p[i] = c;
-}
-if (p == NULL)
-{
-free(p);
-exit(1);
 }
 return (p);
 }
