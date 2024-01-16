92101a2cf32a574f050a4df7419329e6361dd8ba
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-16 21:31:13 +0200

    000000

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * create_array - creates an array of chars
+ * @size: parmeter
+ * @c: parameter
+ * Return: pointer
+*/
+char *create_array(unsigned int size, char c)
+{ unsigned int i;
+char *p = malloc(size * sizeof(char));
+if (size == 0)
+{
+return ('\0');
+}
+for (i = 0; i < size; i++)
+{
+p[i] = c;
+}
+return (p);
+}
