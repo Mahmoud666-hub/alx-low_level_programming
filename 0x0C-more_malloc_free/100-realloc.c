fd3627c9f7ecfc1e89fe7a9532640f660d17f2d5
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-21 23:18:39 +0200

    100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,43 @@
+#include"main.h"
+/**
+ * _realloc - reallocate space
+ * @ptr: parameter
+ * @old_size: parameter
+ * @new_size: parameter
+ * Return: pointer
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+ptr = malloc(old_size);
+if (ptr == NULL && new_size == 0)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL && new_size != 0)
+{
+ptr = malloc(new_size);
+return (ptr);
+}
+if (new_size == old_size)
+{
+return (ptr);
+}
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (new_size < old_size)
+{
+free(ptr);
+return (NULL);
+}
+ptr = malloc(new_size);
+if (ptr == NULL)
+{
+free(ptr);
+return (NULL);
+}
+return (ptr);
+}
--- 0x0C-more_malloc_free/nono.c
+++ 0x0C-more_malloc_free/nono.c
@@ -1,7 +1,10 @@
 #include"main.h"
 int main()
 {
-    char *s = "yes";
-    printf("%ld\n", sizeof(s));
+    int x;
+    char y;
+    x = NULL;
+y = NULL;
+printf("number %d", y);
     return (0);
 }
