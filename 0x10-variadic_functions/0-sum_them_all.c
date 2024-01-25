de410ed463e3e0a192f02706f73409a5638bfa29
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-25 22:09:58 +0200

    0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include"variadic_functions.h"
+/**
+ * sum_them_all - function sum integers
+ * @n: parameter
+ * Return: integer
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+int x, i;
+int sum = 0;
+va_list arg;
+if (n == 0)
+{
+return (0);
+}
+va_start(arg, n);
+for (i = 0; i < n; i++)
+{
+x = va_arg(arg, int);
+sum += x;
+}
+va_end(arg);
+return (sum);
+}
