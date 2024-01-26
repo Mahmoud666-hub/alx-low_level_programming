327d5036cae0fd986897e2bc68a8b9cd1d2fd1b5
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-26 19:21:47 +0200

    222-

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,8 +1,27 @@
 #include"function_pointers.h"
 /**
- * 
+ * int_index - give index
+ * @array: parameter
+ * @size: parameter
+ * @cmp: parameter
+ * Return: integer
 */
 int int_index(int *array, int size, int (*cmp)(int))
+{int i;
+if (size <= 0)
 {
-    
+return (-1);
+}
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) == 1)
+{
+return (i);
+}
+else if (cmp(array[i]) == 0 && i == (size - 1))
+{
+return (-1);
+}
+}
+return (i);
 }
---
+++ 0x0F-function_pointers/nono.c
@@ -0,0 +1,21 @@
+#include"function_pointers.h"
+
+int main(int argc, char *argv[])
+{
+	int mul, a, b;
+	//if (argc != 3)
+//{
+	//printf("Error\n");
+	//return (1);
+//}
+
+	//a = atoi(argv[1]);
+	//b = atoi(argv[2]);
+	//mul = a * b;
+	//a = 6;
+	
+
+	printf("%s\n", '\0');
+
+return (0);
+}
