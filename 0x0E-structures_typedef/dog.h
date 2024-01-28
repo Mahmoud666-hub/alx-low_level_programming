831e36745454cacde5ea023e2213d0d498e24cd6
root <root@DESKTOP-K7TTS03.localdomain>
2024-01-29 00:42:10 +0200

    33-

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 #include<stdio.h>
+#include<stdlib.h>
+typedef struct dog dog_t;
 /**
  * struct dog - structure
  * @name: parameter
  * @age: parameter
  * @owner: parameter
 */
-define struct dog
+struct dog
 {
 char *name;
 float age;
 char *owner;
-}dog_t;
+};
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
