ad6a032605540539960e65d21e935351da484957
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-04 02:37:34 +0200

    4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include"dog.h"
+/**
+ * new_dog - create new
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * Return: struct
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *p;
+p = malloc(sizeof(dog_t));
+if (p == NULL)
+{
+return (NULL);
+}
+p->name = name;
+p->age = age;
+p->owner = owner;
+return (p);
+}
