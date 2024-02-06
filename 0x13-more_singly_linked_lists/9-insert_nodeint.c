171b677550cdf3e82926a0503fb2cfe36a580b91
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-07 00:57:55 +0200

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 unsigned int x = 0;
 listint_t *p = *head;
-listint_t *u;
+listint_t *u = NULL;
 if (head == NULL || *head == NULL)
 {
 return (NULL);
@@ -22,6 +22,12 @@ return (NULL);
 }
 u->n = n;
 u->next = NULL;
+if (idx == 0)
+{
+u->next = p;
+(*head) = u;
+return (*head);
+}
 
 while (p->next != NULL && x < (idx - 1))
 {
