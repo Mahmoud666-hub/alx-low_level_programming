94c4103937eb859f9b998815c780b529143e1b08
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-06 09:27:19 +0200

    5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include"lists.h"
+/**
+ * free_listint2 - free list nodes
+ * @head: parameter
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *p;
+if (*head == NULL)
+{
+return;
+}
+if ((*head)->next == NULL)
+{
+free(*head);
+return;
+}
+p = *head;
+while (p != NULL)
+{
+p = p->next;
+free(*head);
+(*head) = p;
+}
+}
