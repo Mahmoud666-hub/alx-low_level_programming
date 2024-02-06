2c0fd3b7114a0f6f6343754618b41cebfb2cb29d
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-06 08:12:34 +0200

    4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include"lists.h"
+/**
+ * free_listint - free list of nodes
+ * @head: parameter
+*/
+void free_listint(listint_t *head)
+{
+listint_t *p;
+if (head->next == NULL)
+{
+free(head);
+return;
+}
+p = head;
+while (p)
+{
+p = p->next;
+free(head);
+head = p;
+}
+}
