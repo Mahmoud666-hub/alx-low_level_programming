79243234f931d5f6b2b4ebd0ae5ab44b6f769c82
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-06 13:13:29 +0200

    5-55

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,13 +6,15 @@
 void free_listint2(listint_t **head)
 {
 listint_t *p;
-if (*head == NULL)
+if (*head == NULL || head == NULL)
 {
 return;
 }
+
 if ((*head)->next == NULL)
 {
 free(*head);
+(*head) = NULL;
 return;
 }
 p = *head;
