2892ca003334c71494b09413ac4636f58c02c639
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-06 13:27:49 +0200

    5-55-5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,6 +8,8 @@ void free_listint2(listint_t **head)
 listint_t *p;
 if (*head == NULL || head == NULL)
 {
+(*head) = NULL;
+p = NULL;
 return;
 }
 
@@ -15,14 +17,15 @@ if ((*head)->next == NULL)
 {
 free(*head);
 (*head) = NULL;
+p = NULL;
 return;
 }
 p = *head;
-while ((*head) != NULL)
+while (p != NULL)
 {
 p = p->next;
 free(*head);
 (*head) = p;
 }
-*head = NULL;
+(*head) = NULL;
 }
