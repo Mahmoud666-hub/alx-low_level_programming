2fb73e1ef81a5f8a94eaf317f2b82f61b3716282
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-06 22:09:45 +0200

    7-7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,7 +9,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int x = 0;
 listint_t *p = head;
-if (head == NULL || index == 0)
+if (head == NULL)
 {
 return (NULL);
 }
