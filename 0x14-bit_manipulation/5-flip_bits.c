ab363c87fd88fcb91c1d82994a4a962a0defa275
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-11 20:17:10 +0200

    5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,29 @@
+#include"main.h"
+/**
+ * flip_bits - no. of different bits
+ * @n: parameter
+ * @m: parameter
+ * Return: integer
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int a, b, c;
+	unsigned int x = 0, i = 0;
+
+	while (m > 0 || n > 0)
+	{
+		a = n >> 1;
+		b = m >> 1;
+		if ((a * 2 == n) == (b * 2 == m))
+		{
+			i++;
+		}
+		else
+		{
+			x++;
+		}
+		n = a;
+		m = b;
+	}
+return (x);
+}
