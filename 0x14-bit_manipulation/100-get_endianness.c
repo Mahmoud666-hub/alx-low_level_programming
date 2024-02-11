973e6cfa5faa2ad71880d0d9a6d297500b1ec58d
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-11 22:03:02 +0200

    100-

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -9,10 +9,14 @@ int get_endianness(void)
 
 	char *c = (char *)&x;/*لازم تعمل type casting*/
 
-	if (c[0] == '1')
+/*printf("%d\n", c[1]);*/
+/*c[0] c[1] c[2] c[3] 1 0 0 0 big endian*/
+
+	if (c[0] == 1)
 	{
-		return (1);
+
+		return (0);
 	}
 	else
-	return (0);
+	return (1);
 }
