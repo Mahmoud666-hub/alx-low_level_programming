0629d08671e749b9e760f3895c861023fb0bcab3
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-15 21:43:39 +0200

    0--0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,17 +20,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-	 while ((y = read(x, buf, sizeof(buf) - 1)) > 0)
-    {
-        buf[y] = '\0';
+	while ((y = read(x, buf, sizeof(buf) - 1)) > 0)
+	{
+		buf[y] = '\0';
 		write(STDOUT_FILENO, buf, y);
-        letters -= y;
-        if (letters <= 0)
-        {
-            break;
-        }
+		letters -= y;
+		if (letters <= 0)
+		{
+			break;
+		}
 		z += y;
-    }
+	}
 	close(x);
 	return (z + 1);
 }
