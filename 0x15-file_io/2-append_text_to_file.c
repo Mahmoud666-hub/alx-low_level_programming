c4a8719f78a2f2c2ba59033a678a039239a010fe
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-17 13:26:12 +0200

    2-

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		fclose(p);
 		return (1);
 	}
-	fputs(text_content, filename);
+	fputs(text_content, p);
 	fclose(p);
 	return (1);
 }
