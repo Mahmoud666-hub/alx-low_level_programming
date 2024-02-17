10a5adaf2974f35e5fad7d48e770e018565f004a
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-17 12:26:24 +0200

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	x = open(filename, O_CREAT | O_RDWR, 0600);
+	x = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (x == -1)
 	{
 		return (-1);
