675b77964e6224c2b424b078ccfe5c2d18537630
mahmoud <ramadanmahmoud945@gmail.com>
2024-02-19 14:28:24 +0200

    so

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,40 +8,34 @@
 int main(int argc, char **argv)
 {
 	FILE *x;
-	int y, e, m;
-	char a[50];
+	int y;
+	char c;
 
 	if (argc != 3)
 	{dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+
 	x = fopen(argv[1], "r");
 	if (x == NULL)
 	{dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
 	}
-	while (!feof(x))
-	{
-		e = fread(a, 1, 1024, x);
-		if (e == -1)
-		{dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE\n");
-			exit(98);
-		}
-	}
+	
+
 	y = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (y == -1)
 	{dprintf(2, "Error: Can't write to NAME_OF_THE_FILE\n");
 		exit(99);
 	}
-	while (m != e)
+	
+	while ((c = fgetc(x)) != EOF )
 	{
-		m = write(y, a, 1024);
-		if (m == -1)
-		{dprintf(2, "Error: Can't write to NAME_OF_THE_FILE\n");
-			exit(99);
-		}
+		write(y, &c, 1);
 	}
-	if (close(y) == -1)
+	
+
+	if (close(y) == -1 || fclose(x) == -1)
 	{dprintf(2, "Error: Can't close fd FD_VALUE\n");
 		exit(100);
 	}
