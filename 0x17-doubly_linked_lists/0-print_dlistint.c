71d6a9cb56d374089218d30cc360040845d126ab
mahmoud <ramadanmahmoud945@gmail.com>
2024-03-24 10:19:11 +0200

    bn

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -15,11 +15,10 @@ size_t print_dlistint(const dlistint_t *h)
 
 	while (h != NULL)
 	{
-		x = printf("%d\n", h->n);
-		m = m + (x - 1);
-		/*printf("x--(%ld\n", x);*/
+		printf("%d\n", h->n);
+		m = m + 1;
 		h = h->next;
 	}
 
-	return (m); /*x = h->n + \n*/
+	return (m);
 }
--- 0x17-doubly_linked_lists/0main.c
+++ 0x17-doubly_linked_lists/0main.c
@@ -12,6 +12,7 @@ int main(void)
 {
     dlistint_t *head;
     dlistint_t *new;
+    dlistint_t *w;
     dlistint_t hello = {8, NULL, NULL};
     size_t n;
 
@@ -27,6 +28,21 @@ int main(void)
     new->next = head;
     new->prev = NULL;
     head = new;
+
+    w = malloc(sizeof(dlistint_t));
+    if (w == NULL)
+    {
+        dprintf(2, "Error: Can't malloc\n");
+        return (EXIT_FAILURE);
+    }
+    w->n = 92;
+    head->prev = w;
+    w->next = head;
+    w->prev = NULL;
+    head = w;
+
+
+
     n = print_dlistint(head);
     printf("-> %lu elements\n", n);
     free(new);
